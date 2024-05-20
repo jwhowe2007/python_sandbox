ec969ac072a32edb65885fe8393d631f8cc03eb1
Justin Howe <jwhowe02@proton.me>
2024-05-19 23:42:56 -0700

    Add some static assets and some templates for the frontend

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, request
+from flask import Flask, url_for, request, render_template
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -27,18 +27,20 @@ def projects():
 
 @app.route('/about')
 def about():
-    return 'The about page'
+    return render_template('about.html')
 
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        return f'Posting login creds...'
-    else:
-        return f'Logged in! Redirecting to about page.'
+@app.get("/login")
+def login_get():
+    return f'Logged in! Redirecting to the About page.'
+
+@app.post("/login")
+def login_post():
+    return f'Posting login creds...'
 
 with app.test_request_context():
     print(url_for('post_index', post_id=7))
     print(url_for('projects'))
-    print(url_for('login'))
-    print(url_for('login', next='/about'))
+    print(url_for('about'))
+    print(url_for('login_get'))
+    print(url_for('login_get', next='/about'))
     print(url_for('profile', username='Rick Sanchez'))
