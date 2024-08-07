783564d1335e7a3715313536c7f6d63978f098b4
Justin Howe <jwhowe02@proton.me>
2024-08-07 03:25:24 -0700

    Add initials ASCII art project source

---
+++ standalone/initials.py
@@ -0,0 +1,25 @@
+# ASCII-art!
+
+# First name and surname: Justin Howe
+# Fun Fact: In addition to learning programming languages, I love learning natural languages and foreign cultures as well!
+
+firstInitial = """
+JJJJJ   H   H
+  J     H   H
+  J     H   H
+  J     HHHHH
+J J     H   H
+J J     H   H
+ JJ     H   H
+"""
+
+guitarPick = """
+(--------)
+ \      /
+  \    /
+   \  /
+    UU
+"""
+
+print(firstInitial)
+print(guitarPick)
