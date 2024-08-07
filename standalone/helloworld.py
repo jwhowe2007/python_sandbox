7a26b134210899d1ee830c55a304c4b92b3a4c63
Justin Howe <jwhowe02@proton.me>
2024-08-07 02:53:01 -0700

    Added standalone scripts directory for testing

---
+++ standalone/helloworld.py
@@ -0,0 +1,9 @@
+# This is my introductory program to raw Python 3!
+# I'd love to try something more advanced later like data visualization, or ML.
+
+"""
+    helloworld.py
+    Meant to demonstrate basic knowledge of running a program in Python.
+"""
+
+print("Hello, World!");
